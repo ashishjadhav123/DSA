15abb04e72c459e1a1979384611e6595cc65dbac
Ashish <jadhavashish028@gmail.com>
2025-06-05 00:31:01 +0530

    Circular Linked List 50-06-2024

--- Linked List/Circular_Linked_List/Circular_Linked_List.py
+++ Linked List/Circular_Linked_List/Circular_Linked_List.py
@@ -10,40 +10,98 @@ class CircularLinkedList:
         self.head = None
 
     def insert_at_beginning(self, data):
-        node = CNode(data=data)
-
+        """Method to insert element at beginning of Linked List"""
         if self.head is None:
-            node.next = node
-            self.head = node
+            self.head = CNode(data=data, next=self.head)
+        else:
+            self.head = CNode(data=data, next=self.head)
+
+            itr = self.head
+
+            while itr.next:
+                itr = itr.next
+
+            itr.next = self.head
+
+    def print_l_list(self):
+        """Method for print all elements from Linked List"""
+        itr = self.head
+        stop_point = self.head
+        counter = 0
+        str_l_list = ''
+        while itr:
+            suffix = ' -> ' if itr.next else ''
+            str_l_list += str(itr.data) + suffix
+            itr = itr.next
+            counter += 1
+            if itr == stop_point:
+                break
+
+        return str_l_list, counter
+
+    def insert_at_end(self, data):
+        """Method to insert element at end of Linked List"""
+        if self.head is None:
+            self.insert_at_beginning(data=data)
             return
 
         itr = self.head
-        while itr.next != self.head:
+        stop_point = self.head
+
+        while itr.next:
             itr = itr.next
-
-        node.next = self.head
-        itr.next = node
-        self.head = node
-
-    def print_linked_list(self):
-
-        if self.head is None:
-            return "", 0
-
-        itr = self.head
-        str_c_list = ""
-        counter = 0
-
-        while True:
-            str_c_list += str(itr.data)
-            counter += 1
-            itr = itr.next
-            if itr != self.head:
-                str_c_list += " ⇄ "
-            else:
+            if itr.next == stop_point:
                 break
 
-        return str_c_list, counter
+        itr.next = CNode(data=data, next=self.head)
+
+    def insert_at_loc(self, loc, data):
+        """Method to insert element at specific index of Linked List"""
+        if loc < 0 or loc > (self.print_l_list()[1]):
+            raise Exception("Invalid loc")
+
+        if loc == 0:
+            self.insert_at_beginning(data=data)
+            return
+
+        itr = self.head
+        counter = 0
+        while itr:
+            if counter == (loc - 1):
+                itr.next = CNode(data=data, next=itr.next)
+                break
+            counter += 1
+            itr = itr.next
+
+    def remove_at_loc(self, loc):
+        """Method to remove element at specific Location of Linked List"""
+        if loc < 0 or loc > (self.print_l_list()[1]):
+            raise Exception("Invalid loc")
+
+        if loc == 0:
+
+            itr = self.head
+            stop_point = self.head
+
+            self.head = self.head.next
+
+            while itr.next:
+                itr = itr.next
+                if itr.next == stop_point:
+                    break
+
+            itr.next = self.head
+            return
+
+        itr = self.head
+        counter = 0
+
+        while itr:
+            if counter == (loc - 1):
+                itr.next = itr.next.next
+                break
+            counter += 1
+            itr = itr.next
 
 
 if __name__ == '__main__':
@@ -52,4 +110,27 @@ if __name__ == '__main__':
     root.insert_at_beginning(5)
     root.insert_at_beginning(10)
 
-    print(root.print_linked_list()[0])
+    print(root.print_l_list()[0])
+    print(f"Length of Circular Linked list : {root.print_l_list()[1]}")
+
+    root.insert_at_end(15)
+    root.insert_at_end(20)
+
+    print(root.print_l_list()[0])
+    print(f"Length of Circular Linked list : {root.print_l_list()[1]}")
+
+    root.insert_at_loc(loc=2, data=25)
+    root.insert_at_loc(loc=5, data=30)
+
+    print(root.print_l_list()[0])
+    print(f"Length of Circular Linked list : {root.print_l_list()[1]}")
+
+    root.remove_at_loc(loc=2)
+
+    print(root.print_l_list()[0])
+    print(f"Length of Circular Linked list : {root.print_l_list()[1]}")
+
+    root.remove_at_loc(loc=0)
+
+    print(root.print_l_list()[0])
+    print(f"Length of Circular Linked list : {root.print_l_list()[1]}")
