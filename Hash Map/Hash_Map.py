d5d959c2fa9667bb3e7b6940d669621e35e16b7e
Ashish <jadhavashish028@gmail.com>
2025-06-08 02:29:09 +0530

    Hash Map add and delete method 08-06-2025

--- Hash Map/Hash_Map.py
+++ Hash Map/Hash_Map.py
@@ -16,9 +16,31 @@ class HashMap:
                 return
         self.map[h].append([key, value])
 
+    def get(self, key):
+        h = self.get_hash(key=key)
+
+        for pair in self.map[h]:
+            if pair[0] == key:
+                return pair[1]
+
+        return None
+
+
+    def delete(self, key):
+
+        h = self.get_hash(key=key)
+
+        for i, pair in enumerate(self.map[h]):
+            if pair[0] == key:
+                del self.map[h][i]
+                return True
+        return False
+
     def print_hash_map(self):
         for i in range(self.size):
             print(i, " : ", self.map[i])
+
+        print('\n')
 
 
 if __name__ == "__main__":
@@ -30,5 +52,11 @@ if __name__ == "__main__":
     root.add(key='key 4', value=20)
     root.add(key='key 5', value=25)
     root.add(key='key 6', value=30)
+
+    root.print_hash_map()
+
+    print(root.get(key='key 4'))
+
+    root.delete(key='key 4')
 
     root.print_hash_map()
