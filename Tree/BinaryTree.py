965ad175f732e65f0348f4b6f8da67f5eed72cad
Ashish <jadhavashish028@gmail.com>
2025-06-16 00:06:27 +0530

    Binary Tree in_Order_Traversal 15-06-2025

--- Tree/BinaryTree.py
+++ Tree/BinaryTree.py
@@ -22,6 +22,23 @@ class BinarySearchTree:
             else:
                 self.right = BinarySearchTree(data=data)
 
+    def in_order_traversal(self):
+        elements = []
+
+        # Insert Left nodes
+        if self.left:
+            left_elements = self.left.in_order_traversal()
+            elements += left_elements
+
+        elements.append(self.data)
+
+        # Insert Right nodes
+        if self.right:
+            right_elements = self.right.in_order_traversal()
+            elements += right_elements
+
+        return elements
+
 
 def build_tree(elements):
 
@@ -36,4 +53,6 @@ def build_tree(elements):
 if __name__ == "__main__":
     numbers = [10, 6, 12, 3, 15, 8, 17, 11]
     tree = build_tree(elements=numbers)
+
+    print(tree.in_order_traversal())
     pass
