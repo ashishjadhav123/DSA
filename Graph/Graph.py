69c6903ad21a072474c0f5e2ef6d9a3ce5feb58d
Ashish <jadhavashish028@gmail.com>
2025-06-20 00:42:28 +0530

    Graph 20-06-2025

---
+++ Graph/Graph.py
@@ -0,0 +1,46 @@
+
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = {}
+        for start, end in self.edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+
+        print(self.graph_dict)
+
+    def get_path(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+
+        if start not in self.graph_dict:
+            return []
+
+        paths = []
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_path = self.get_path(start=node, end=end, path=path)
+                for p in new_path:
+                    paths.append(p)
+
+        return paths
+
+
+if __name__ == "__main__":
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto")
+    ]
+
+    root = Graph(edges=routes)
+
+    print(root.get_path(start="Toronto", end="Dubai"))
+    print(root.get_path(start="Mumbai", end="New York"))
